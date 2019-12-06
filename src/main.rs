ab5e7ddb99f8d199aa1a3b526866fdc9a5cceffa
Jason Martens <jmartens@zendesk.com>
2019-12-06 14:09:43 -0800

    Try using async example from rust-rdkafka

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,152 @@
-use rdkafka::ClientConfig;
+use std::thread;
+use std::time::Duration;
 
-fn main() {
-    println!("Hello, world!");
-    let context = CustomContext();
-    let consumer: ProcessingConsumer = ClientConfig
+use clap::{App, Arg};
+use futures::{future, TryStreamExt};
+use log::{info, warn};
+
+use rdkafka::config::ClientConfig;
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::Consumer;
+use rdkafka::message::OwnedMessage;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::Message;
+
+use crate::example_utils::setup_logger;
+
+mod example_utils;
+
+// Emulates an expensive, synchronous computation.
+fn expensive_computation<'a>(msg: OwnedMessage) -> String {
+    info!("Starting expensive computation on message {}", msg.offset());
+    thread::sleep(Duration::from_millis(rand::random::<u64>() % 5000));
+    info!(
+        "Expensive computation completed on message {}",
+        msg.offset()
+    );
+    match msg.payload_view::<str>() {
+        Some(Ok(payload)) => format!("Payload len for {} is {}", payload, payload.len()),
+        Some(Err(_)) => "Message payload is not a string".to_owned(),
+        None => "No payload".to_owned(),
+    }
+}
+
+// Creates all the resources and runs the event loop. The event loop will:
+//   1) receive a stream of messages from the `StreamConsumer`.
+//   2) filter out eventual Kafka errors.
+//   3) send the message to a thread pool for processing.
+//   4) produce the result to the output topic.
+// `tokio::spawn` is used to handle IO-bound tasks in parallel (e.g., producing
+// the messages), while `tokio::task::spawn_blocking` is used to handle the
+// simulated CPU-bound task.
+async fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_topic: &str) {
+    // Create the `StreamConsumer`, to receive the messages from the topic in form of a `Stream`.
+    let consumer: StreamConsumer = ClientConfig::new()
         .set("group.id", group_id)
         .set("bootstrap.servers", brokers)
-        .set("enable.auto.commit", "true")
-        .set("enable.auto.offset.store", "false")
-        .set("statistics.interval.ms", "0")
-        .set("fetch.error.backoff.ms", "1")
-        .set("auto.offset.reset", "earliest")
-        .set_log_level(dbg!(RDKafkaLogLevel))
-        .optionally_set_ssl_from_env()
-        .create_with_context(context)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "false")
+        .create()
         .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&[input_topic])
+        .expect("Can't subscribe to specified topic");
+
+    // Create the `FutureProducer` to produce asynchronously.
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", brokers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation error");
+
+    // Create the outer pipeline on the message stream.
+    let stream_processor = consumer.start().try_for_each(|borrowed_message| {
+        // Process each message
+        info!("Message received: {}", borrowed_message.offset());
+        // Borrowed messages can't outlive the consumer they are received from, so they need to
+        // be owned in order to be sent to a separate thread.
+        let owned_message = borrowed_message.detach();
+        let output_topic = output_topic.to_string();
+        let producer = producer.clone();
+        tokio::spawn(async move {
+            // The body of this block will be executed on the main thread pool,
+            // but we perform `expensive_computation` on a separate thread pool
+            // for CPU-intensive tasks via `tokio::task::spawn_blocking`.
+            let computation_result =
+                tokio::task::spawn_blocking(|| expensive_computation(owned_message))
+                    .await
+                    .expect("failed to wait for expensive computation");
+            let produce_future = producer.send(
+                FutureRecord::to(&output_topic)
+                    .key("some key")
+                    .payload(&computation_result),
+                0,
+            );
+            match produce_future.await {
+                Ok(Ok(delivery)) => println!("Sent: {:?}", delivery),
+                Ok(Err((e, _))) => println!("Error: {:?}", e),
+                Err(_) => println!("Future cancelled"),
+            }
+        });
+        future::ready(Ok(()))
+    });
+
+    info!("Starting event loop");
+    stream_processor.await.expect("stream processing failed");
+    info!("Stream processing terminated");
+}
+
+#[tokio::main]
+async fn main() {
+    let matches = App::new("Async example")
+        .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
+        .about("Asynchronous computation example")
+        .arg(
+            Arg::with_name("brokers")
+                .short("b")
+                .long("brokers")
+                .help("Broker list in kafka format")
+                .takes_value(true)
+                .default_value("localhost:9092"),
+        )
+        .arg(
+            Arg::with_name("group-id")
+                .short("g")
+                .long("group-id")
+                .help("Consumer group id")
+                .takes_value(true)
+                .default_value("example_consumer_group_id"),
+        )
+        .arg(
+            Arg::with_name("log-conf")
+                .long("log-conf")
+                .help("Configure the logging format (example: 'rdkafka=trace')")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("input-topic")
+                .long("input-topic")
+                .help("Input topic")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("output-topic")
+                .long("output-topic")
+                .help("Output topic")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    setup_logger(true, matches.value_of("log-conf"));
+
+    let brokers = matches.value_of("brokers").unwrap();
+    let group_id = matches.value_of("group-id").unwrap();
+    let input_topic = matches.value_of("input-topic").unwrap();
+    let output_topic = matches.value_of("output-topic").unwrap();
+
+    run_async_processor(brokers, group_id, input_topic, output_topic).await
 }
