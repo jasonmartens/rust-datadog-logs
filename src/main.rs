8b8f9138de9f7125edfbb180c13cce515378fed3
Jason Martens <jmartens@zendesk.com>
2019-12-06 15:33:08 -0800

    Remove the kafka producer & add reqwest and serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
-#[macro_use]
-extern crate log;
 extern crate clap;
 extern crate futures;
+#[macro_use]
+extern crate log;
 extern crate rand;
 extern crate rdkafka;
 extern crate tokio;
 
 use clap::{App, Arg};
-use futures::{lazy, Future, Stream};
+use futures::{lazy, Stream};
+use rdkafka::config::ClientConfig;
+use rdkafka::consumer::Consumer;
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::Message;
+use rdkafka::message::OwnedMessage;
+use serde_json::{Error, Value};
 use tokio::runtime::current_thread;
 
-use rdkafka::config::ClientConfig;
-use rdkafka::consumer::stream_consumer::StreamConsumer;
-use rdkafka::consumer::Consumer;
-use rdkafka::message::OwnedMessage;
-use rdkafka::producer::{FutureProducer, FutureRecord};
-use rdkafka::Message;
-
-use std::thread;
-use std::time::Duration;
+use crate::example_utils::setup_logger;
 
 mod example_utils;
-use crate::example_utils::setup_logger;
 
 // Emulates an expensive, synchronous computation.
 fn expensive_computation<'a>(msg: OwnedMessage) -> String {
-    info!("Starting expensive computation on message {}", msg.offset());
-    thread::sleep(Duration::from_millis(rand::random::<u64>() % 5000));
-    info!(
-        "Expensive computation completed on message {}",
-        msg.offset()
-    );
     match msg.payload_view::<str>() {
-        Some(Ok(payload)) => format!("Payload len for {} is {}", payload, payload.len()),
+        Some(Ok(payload)) => {
+            let parsed: Result<Value, Error> = serde_json::from_str(payload);
+            match parsed {
+                Ok(json) => {
+                    format!("Payload timestamp is {} for offset {}", json["@timestamp"], msg.offset())
+                }
+                Err(err) => format!("Failed to parse json. Error: {}", err)
+            }
+        }
         Some(Err(_)) => "Message payload is not a string".to_owned(),
         None => "No payload".to_owned(),
     }
@@ -42,11 +41,10 @@ fn expensive_computation<'a>(msg: OwnedMessage) -> String {
 //   1) receive a stream of messages from the `StreamConsumer`.
 //   2) filter out eventual Kafka errors.
 //   3) send the message to a thread pool for processing.
-//   4) produce the result to the output topic.
 // Moving each message from one stage of the pipeline to next one is handled by the event loop,
 // that runs on a single thread. The expensive CPU-bound computation is handled by the `ThreadPool`,
 // without blocking the event loop.
-fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_topic: &str) {
+fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str) {
     // Create the `StreamConsumer`, to receive the messages from the topic in form of a `Stream`.
     let consumer: StreamConsumer = ClientConfig::new()
         .set("group.id", group_id)
@@ -61,13 +59,6 @@ fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_
         .subscribe(&[input_topic])
         .expect("Can't subscribe to specified topic");
 
-    // Create the `FutureProducer` to produce asynchronously.
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", brokers)
-        .set("message.timeout.ms", "5000")
-        .create()
-        .expect("Producer creation error");
-
     // Create the runtime where the expensive computation will be performed.
     let mut thread_pool = tokio::runtime::Builder::new()
         .name_prefix("pool-")
@@ -77,7 +68,7 @@ fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_
 
     // Use the current thread as IO thread to drive consumer and producer.
     let mut io_thread = current_thread::Runtime::new().unwrap();
-    let io_thread_handle = io_thread.handle();
+//    let io_thread_handle = io_thread.handle();
 
     // Create the outer pipeline on the message stream.
     let stream_processor = consumer
@@ -98,28 +89,10 @@ fn run_async_processor(brokers: &str, group_id: &str, input_topic: &str, output_
             // Borrowed messages can't outlive the consumer they are received from, so they need to
             // be owned in order to be sent to a separate thread.
             let owned_message = borrowed_message.detach();
-            let output_topic = output_topic.to_string();
-            let producer = producer.clone();
-            let io_thread_handle = io_thread_handle.clone();
             let message_future = lazy(move || {
                 // The body of this closure will be executed in the thread pool.
                 let computation_result = expensive_computation(owned_message);
-                let producer_future = producer
-                    .send(
-                        FutureRecord::to(&output_topic)
-                            .key("some key")
-                            .payload(&computation_result),
-                        0,
-                    )
-                    .then(|result| {
-                        match result {
-                            Ok(Ok(delivery)) => println!("Sent: {:?}", delivery),
-                            Ok(Err((e, _))) => println!("Error: {:?}", e),
-                            Err(_) => println!("Future cancelled"),
-                        }
-                        Ok(())
-                    });
-                let _ = io_thread_handle.spawn(producer_future);
+                info!("Computation result: {}", computation_result);
                 Ok(())
             });
             thread_pool.spawn(message_future);
@@ -164,13 +137,6 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
-        .arg(
-            Arg::with_name("output-topic")
-                .long("output-topic")
-                .help("Output topic")
-                .takes_value(true)
-                .required(true),
-        )
         .get_matches();
 
     setup_logger(true, matches.value_of("log-conf"));
@@ -178,7 +144,6 @@ fn main() {
     let brokers = matches.value_of("brokers").unwrap();
     let group_id = matches.value_of("group-id").unwrap();
     let input_topic = matches.value_of("input-topic").unwrap();
-    let output_topic = matches.value_of("output-topic").unwrap();
 
-    run_async_processor(brokers, group_id, input_topic, output_topic);
+    run_async_processor(brokers, group_id, input_topic);
 }
